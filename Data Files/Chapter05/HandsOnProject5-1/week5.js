9f750abeb9b6485c6894ff03b3da2a003c8f869d
Blessing Ajiboye <blessjiboye@yahoo.com>
2023-05-09 16:15:46 -0400

    first commit

---
+++ Data Files/Chapter05/HandsOnProject5-1/week5.js
@@ -0,0 +1,6 @@
+"use strict";
+document.getElementsByTagName("p")[0].innerHTML = "web browser name:" + navigator.appName;
+document.getElementsByTagName("p")[1].innerHTML = "web browser version: " + navigator.appVersion;
+document.getElementsByTagName("p")[2].innerHTML = "Operating platform:" + navigator.platform;
+document.getElementsByTagName("p")[3].innerHTML = "User agent: " + navigator.userAgent;
+
---
+++ Solution Files/Solution Files/Chapter01_Solutions/Chapter/plants.js
@@ -0,0 +1,14 @@
+﻿/* JavaScript 6th Edition
+Chapter 1
+Chapter Case
+
+Tinley Xeriscapes
+Author: Blessing Ajiboye
+Date:  26tt December 2016
+
+Filename: plants.js
+*/
+//define variables containing img src values
+var blanket = "images/blanket.jpg";
+var bluestem = "images/bluestem.jpg";
+var rugosa = "images/rugosa.jpg";
